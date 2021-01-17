d426f0bce1601d404a2cd0042d100b390e73a6bd
madcoder1000000 <39967871+madcoder1000000@users.noreply.github.com>
2021-01-16 19:30:40 -0500

    Update LocalScript.lua

--- UI/Frame/TextButton/LocalScript.lua
+++ UI/Frame/TextButton/LocalScript.lua
@@ -2,11 +2,20 @@ function scan(inst)
 	for i,item in pairs(inst:GetDescendants()) do
 		if item:IsA("ModuleScript") or item:IsA("LocalScript") or item:IsA("Script") then
 			if string.find(item.Source, script.Parent.Parent.TextBox.Text) ~= nil then
-				local label = Instance.new("TextLabel", script.Parent.Parent.Parent.ScriptList.ScrollingFrame)
+				local label = Instance.new("TextButton", script.Parent.Parent.Parent.ScriptList.ScrollingFrame)
 				label.Size = UDim2.new(1,0,0.05,0)
-				label.Text = inst.Name.." | "..item.Name
+				label.Text = inst.Name.." | "..item.Name.." -Click to quarantine"
 				label.BackgroundTransparency = 1
 				label.TextColor3 = Color3.new(1,1,1)
+				label.MouseButton1Click:Connect(function()
+					if game.ServerStorage:FindFirstChild("MadCheck_quarantine") then
+						item.Disabled = true
+						item.Parent = game.ServerStorage.MadCheck_quarantine
+					else
+						local folder = Instance.new("Folder", game.ServerStorage)
+						folder.Name = "MadCheck_quarantine"
+					end
+				end)
 			end
 		end
 	end
